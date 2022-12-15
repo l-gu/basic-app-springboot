946f3b70ee98e035cd90358d867cf53eaf2ac21e
l-gu <laurent@telosys.org>
2022-12-15 16:15:19 +0100

    Comments added

--- src/main/java/org/demo/myapp/persistence/repositories/BookRepository.java
+++ src/main/java/org/demo/myapp/persistence/repositories/BookRepository.java
@@ -6,12 +6,28 @@ import java.util.List;
 import org.demo.myapp.persistence.entities.Book;
 import org.springframework.data.repository.PagingAndSortingRepository;
 
+/**
+ * Spring Data JPA repository for entity "xx" <br> 
+ * 
+ * This repository extends PagingAndSortingRepository interface <br>
+ * so it provides by default all the basic CRUD operations : <br>
+ *   findById, findAll, save, delete, etc <br> 
+ * with pagination and sorting : <br>
+ *   findAll(Pageable), findAll(Sort)<br>
+ * 
+ * This repository can be extended by adding specific "finders" methods<br>
+ * To do so, see the "predicates conventions" for "derived query methods" in Spring Data documentation
+ * 
+ * @author xx
+ *
+ */
 public interface BookRepository extends PagingAndSortingRepository<Book, Long> {
 
+	// Insert specific finders here 
 	List<Book> findByTitle(String title);
-	
+
 	List<Book> findByTitleStartingWith(String titlePart);
-	
+
 	List<Book> findByTitleContaining(String titlePart);
 
 	List<Book> findByPrice(BigDecimal price);
--- src/main/java/org/demo/myapp/rest/services/BookService.java
+++ src/main/java/org/demo/myapp/rest/services/BookService.java
@@ -12,16 +12,24 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
 
+/**
+ * REST service for entity "Book" <br>
+ * 
+ * This service provides all the necessary operations required by the REST controller<br>
+ * 
+ * @author 
+ *
+ */
 @Service
 public class BookService extends GenericService<Book, BookRestDTO> {
 
-	// Standard logger (not static => so it can be reused without having to change the name of the class)
-	private final Logger logger = LoggerFactory.getLogger(this.getClass());
+	private static final Logger logger = LoggerFactory.getLogger(BookService.class);
 
 	private final BookRepository bookRepository; // injected by constructor
 
 	/**
 	 * Constructor usable for Dependency Injection
+	 * 
 	 * @param bookRepository the repository to be injected
 	 */
 	public BookService(BookRepository bookRepository) {
@@ -29,121 +37,125 @@ public class BookService extends GenericService<Book, BookRestDTO> {
 		this.bookRepository = bookRepository;
 	}
 
+	/**
+	 * Finds all occurrences of the entity
+	 * @return
+	 */
 	public List<BookRestDTO> findAll() {
-		logger.debug("findAll");
+		logger.debug("findAll()");
 		Iterable<Book> all = bookRepository.findAll();
 		return entityListToDtoList(all);
 	}
 
+	/**
+	 * Finds the entity identified by the given PK
+	 * @param id
+	 * @return the entity or null if not found
+	 */
 	public BookRestDTO findById(long id) {
-		logger.debug("findBook {}", id);
+		logger.debug("findBook({})", id);
 		Optional<Book> optionalBook = bookRepository.findById(id);
 		return entityToDto(optionalBook);
 	}
 
+	/**
+	 * Saves the given entity with the given PK (updated if exists else created)
+	 * @param id
+	 * @param dto 
+	 */
 	public void save(long id, BookRestDTO dto) {
-		logger.debug("save {} : {}", id, dto);
-		dto.setId(id);
-		bookRepository.save(dtoToEntity(dto)); 
+		logger.debug("save({},{})", id, dto);
+		dto.setId(id); 
+		bookRepository.save(dtoToEntity(dto));
 	}
-	
+
+	/**
+	 * Updates the given entity if it exists
+	 * @param dto
+	 * @return true if updated, false if not found
+	 */
 	public boolean update(BookRestDTO dto) {
-		logger.debug("update {}", dto);
-		if ( bookRepository.existsById(dto.getId()) ) {
-			bookRepository.save(dtoToEntity(dto)); 
+		logger.debug("update({})", dto);
+		if (bookRepository.existsById(dto.getId())) {
+			bookRepository.save(dtoToEntity(dto));
 			return true; // find and updated
-		}
-		else {
-			return false; // not found (not updated)
-		}
-	}
-	
-	public boolean partialUpdate(long id, BookRestDTO dto) {
-		logger.debug("partialUpdate {} : {}", id, dto);
-		Optional<Book> optionalBook = bookRepository.findById(id);
-		if ( optionalBook.isPresent() ) {
-			Book book = optionalBook.get();
-			// implement here the partial update
-			bookRepository.save(book);
-			return true; // find and updated
-		}
-		else {
+		} else {
 			return false; // not found (not updated)
 		}
 	}
 
-	public boolean create(BookRestDTO dto) {
-		logger.debug("create {}", dto);
-		if ( bookRepository.existsById(dto.getId()) ) {
-			return false; // already exists, not created
+	/**
+	 * Updates partially the given entity if it exists
+	 * @param id
+	 * @param dto
+	 * @return true if updated, false if not found
+	 */
+	public boolean partialUpdate(long id, BookRestDTO dto) {
+		logger.debug("partialUpdate({}, {})", id, dto);
+		Optional<Book> optionalBook = bookRepository.findById(id);
+		if (optionalBook.isPresent()) {
+			Book book = optionalBook.get();
+			// implement here the partial update
+			// book.setXxx(dto.getXxx());
+			bookRepository.save(book);
+			return true; // find and updated
+		} else {
+			return false; // not found (not updated)
 		}
-		else {
-			bookRepository.save(dtoToEntity(dto)); 
+	}
+
+	/**
+	 * Creates the given entity
+	 * @param dto
+	 * @return true if created, false if already exists
+	 */
+	public boolean create(BookRestDTO dto) {
+		logger.debug("create({})", dto);
+		if (bookRepository.existsById(dto.getId())) {
+			return false; // already exists, not created
+		} else {
+			bookRepository.save(dtoToEntity(dto));
 			return true; // created
 		}
 	}
-	
+
+	/**
+	 * Deletes an entity by its PK
+	 * @param id
+	 * @return true if deleted, false if not found
+	 */
 	public boolean deleteById(long id) {
-		logger.debug("deleteById {}", id);
-		if ( bookRepository.existsById(id) ) {
-			bookRepository.deleteById(id); 
+		logger.debug("deleteById({})", id);
+		if (bookRepository.existsById(id)) {
+			bookRepository.deleteById(id);
 			return true; // find and deleted
-		}
-		else {
+		} else {
 			return false; // not found (not deleted)
 		}
 	}
-	
 
-	//-----------------------------------------------------------------------------------------
+	// -----------------------------------------------------------------------------------------
 	// Specific "finders"
-	//-----------------------------------------------------------------------------------------
-	
+	// -----------------------------------------------------------------------------------------
+
 	public List<BookRestDTO> findByTitle(String title) {
-		logger.debug("findByTitle {}", title);
-		//List<Book> list = bookRepository.findByTitle(title);
+		logger.debug("findByTitle({})", title);
+		// List<Book> list = bookRepository.findByTitle(title);
 		List<Book> list = bookRepository.findByTitleContaining(title);
 		return entityListToDtoList(list);
 	}
 
 	public List<BookRestDTO> findByPrice(BigDecimal price) {
-		logger.debug("findByPrice {}", price);
-		//List<Book> list = bookRepository.findByTitle(title);
+		logger.debug("findByPrice({})", price);
+		// List<Book> list = bookRepository.findByTitle(title);
 		List<Book> list = bookRepository.findByPrice(price);
 		return entityListToDtoList(list);
 	}
 
 	public List<BookRestDTO> findByTitleAndPrice(String title, BigDecimal price) {
-		logger.debug("findByTitleAndPrice {} {}", title, price);
+		logger.debug("findByTitleAndPrice({}, {})", title, price);
 		List<Book> list = bookRepository.findByTitleContainingAndPrice(title, price);
 		return entityListToDtoList(list);
 	}
 
-
-//	//-----------------------------------------------------------------------------------------
-//	//-----------------------------------------------------------------------------------------
-//	//--------------- Generic => in super class
-//	protected Book dtoToEntity(BookRestDTO dto) {
-//		return BookMapper.getInstance().dtoToEntity(dto);
-//	}
-//	protected BookRestDTO entityToDto(Book entity) {
-//		return BookMapper.getInstance().entityToDto(entity);
-//	}
-//	protected BookRestDTO entityToDto(Optional<Book> optionalEntity) {
-//		if ( optionalEntity.isPresent()) {
-//			return entityToDto(optionalEntity.get());
-//		}
-//		else {
-//			return null;
-//		}
-//	}
-//	protected List<BookRestDTO> entityListToDtoList(Iterable<Book> entities) {
-//		List<BookRestDTO> dtoList = new LinkedList<>();
-//		if ( entities != null ) {
-//			for ( Book e : entities ) {
-//				dtoList.add(entityToDto(e));
-//			}
-//		}
-//		return dtoList;
-//	}
 }
